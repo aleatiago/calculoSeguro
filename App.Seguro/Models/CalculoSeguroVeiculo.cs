85b09329dec362971fed0b2fc2a14788b93b6710
aleatiago <alexandreatiago@gmail.com>
2020-07-03 14:03:39 -0300

    Add files via upload

---
+++ App.Seguro/Models/CalculoSeguroVeiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teste.Models
+{
+    public class CalculoSeguroVeiculo
+    {
+        public Guid IdCalculo { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+        public string Marca_Modelo { get; set; }
+        public double ValorVeiculo { get; set; }
+        public double Margem { get; set; }
+        public double Lucro { get; set; }
+        public double TaxaRisco { get; set; }
+        public double PremioPuro { get; set; }
+        public double PremioComercial { get; set; }
+        public DateTime DataCalculo { get; set; }
+    }
+}
